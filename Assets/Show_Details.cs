7421a9c4ab68da20b2f11d65a43ad49d070f89bb
Arno <arno.lehtonen@gmail.com>
2016-11-08 13:22:00 +0200

    (Barely) functional map  markers.

---
+++ Assets/Show_Details.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VRTK
+{
+    public class Show_Details : VRTK.VRTK_InteractableObject
+    {
+
+        public GameObject Tooltip;
+
+        protected override void Start()
+        {
+            Tooltip.SetActive(false);
+        }
+
+        override public void StartTouching(GameObject currentTouchingObject)
+        {
+            if (!touchingObjects.Contains(currentTouchingObject))
+            {
+                touchingObjects.Add(currentTouchingObject);
+                OnInteractableObjectTouched(SetInteractableObjectEvent(currentTouchingObject));
+                Tooltip.SetActive(true);
+            }
+        }
+
+
+
+    }
+}
